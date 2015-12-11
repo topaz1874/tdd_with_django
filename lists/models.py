bc19d95f38b6ca422881ebc5163ef7f7d30f6385
topaz1874 <topaz1874@gmail.com>
2015-12-11 22:30:45 +0800

    adjusting models

--- lists/models.py
+++ lists/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class List(models.Model):
+    pass
 
+    
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
     def __unicode__(self):
         return str(self.text)
+
